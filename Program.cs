6660c37b97373c72d7de9340d837d4b5c4c6524b
RanYQB <raniayaqoubi1995@gmail.com>
2025-01-14 18:54:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((options) => {
+    options.AddPolicy("DevCors", (corsBuilder) => {
+        // Application React
+        corsBuilder.WithOrigins("http://localhost:5173")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) => {
+        corsBuilder.WithOrigins("https://sitedeprod.fr")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configuration environnement de développement
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCors");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else 
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
+
+app.MapControllers();
+
+app.Run();
+
